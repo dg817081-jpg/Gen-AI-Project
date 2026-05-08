b2a67dd12466bbc49c8d46e9236d489c45458625
Dhruv Gupta <dg817081@gmail.com>
2026-05-08 17:02:40 +0530

    Add root and health routes to app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,16 +6,29 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
 
-/*require all the routes here */
+// Root Route
+app.get("/", (req, res) => {
+    res.send("Gen AI Backend Running Successfully 🚀")
+})
+
+// Health Route
+app.get("/health", (req, res) => {
+    res.json({
+        status: "ok"
+    })
+})
+
+/* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const { interviewRouter } = require("./routes/interview.routes")
 
-/*using all the routes here */
+/* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
