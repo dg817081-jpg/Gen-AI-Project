f548afe66815206b4d5c0532db227fa1910832be
Dhruv Gupta <dg817081@gmail.com>
2026-05-09 10:47:45 +0530

    Change CORS origin to localhost for development

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "https://gen-ai-project-7-7bhc.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
