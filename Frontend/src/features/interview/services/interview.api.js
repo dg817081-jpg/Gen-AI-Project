72e017e176533a0e6ef1943cb8e8dad7ef5b1627
Dhruv Gupta <dg817081@gmail.com>
2026-05-09 10:17:50 +0530

    Update API base URL to use environment variable

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
-
 
 /**
  * @description Service to generate interview report based on user self description, resume and job description.
@@ -12,6 +11,7 @@ const api = axios.create({
 export const generateInterviewReport = async ({ jobDescription, selfDescription, resumeFile }) => {
 
     const formData = new FormData()
+
     formData.append("jobDescription", jobDescription)
     formData.append("selfDescription", selfDescription)
     formData.append("resume", resumeFile)
@@ -23,7 +23,6 @@ export const generateInterviewReport = async ({ jobDescription, selfDescription,
     })
 
     return response.data
-
 }
 
 
@@ -31,6 +30,7 @@ export const generateInterviewReport = async ({ jobDescription, selfDescription,
  * @description Service to get interview report by interviewId.
  */
 export const getInterviewReportById = async (interviewId) => {
+
     const response = await api.get(`/api/interview/report/${interviewId}`)
 
     return response.data
@@ -41,6 +41,7 @@ export const getInterviewReportById = async (interviewId) => {
  * @description Service to get all interview reports of logged in user.
  */
 export const getAllInterviewReports = async () => {
+
     const response = await api.get("/api/interview/")
 
     return response.data
@@ -51,9 +52,14 @@ export const getAllInterviewReports = async () => {
  * @description Service to generate resume pdf based on user self description, resume content and job description.
  */
 export const generateResumePdf = async ({ interviewReportId }) => {
-    const response = await api.post(`/api/interview/resume/pdf/${interviewReportId}`, null, {
-        responseType: "blob"
-    })
+
+    const response = await api.post(
+        `/api/interview/resume/pdf/${interviewReportId}`,
+        null,
+        {
+            responseType: "blob"
+        }
+    )
 
     return response.data
 }
