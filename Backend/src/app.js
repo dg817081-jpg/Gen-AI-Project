727c4a9187804200d09a0720421fcf7b2f1d2bed
Dhruv Gupta <dg817081@gmail.com>
2026-06-05 12:26:58 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,10 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://gen-ai-project-11.onrender.com"
+    ],
     credentials: true
 }))
 
