113aa1d21ac9745f440c76487222bd61e7e21f85
Dhruv Gupta <dg817081@gmail.com>
2026-06-05 18:35:28 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -51,9 +51,12 @@ export const getAllInterviewReports = async () => {
  * @description Service to generate resume pdf based on user self description, resume content and job description.
  */
 export const generateResumePdf = async ({ interviewReportId }) => {
-    const response = await api.post(`/api/interview/resume/pdf/${interviewReportId}`, null, {
-        responseType: "blob"
-    })
+    const response = await api.get(
+        `/api/interview/resume/pdf/${interviewReportId}`,
+        {
+            responseType: "blob"
+        }
+    )
 
     return response.data
 }
