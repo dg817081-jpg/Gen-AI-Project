e5340ca59758df77074b488aef49a6644a186664
Dhruv Gupta <dg817081@gmail.com>
2026-06-05 18:06:32 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -38,7 +38,12 @@ async function registerUserController(req,res){
         { expiresIn: "1d" }
     );
     
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000
+})
     
     res.status(201).json({
         message: "User registered successfully",
@@ -80,7 +85,12 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     );
     
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000
+})
     
     res.status(200).json({
         message: "User logged in successfully",
@@ -104,7 +114,11 @@ async function logoutUserController(req, res) {
         await tokenBlacklistModel.create({token})
     }
 
-    res.clearCookie("token");
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None"
+});
     
     res.status(200).json({
         message: "User logged out successfully"
